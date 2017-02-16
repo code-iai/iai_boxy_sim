a21a0f2076caa2e5a975883c952fb56a28614067
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2017-02-16 11:36:51 +0100

    More progress on the fire hose controller.

--- include/iai_boxy_sim/fire_hose_controller.h
+++ include/iai_boxy_sim/fire_hose_controller.h
@@ -4,6 +4,8 @@
 #include <ros/ros.h>
 #include <controller_interface/controller.h>
 #include <hardware_interface/joint_command_interface.h>
+#include <giskard_msgs/SemanticFloat64Array.h>
+#include <realtime_tools/realtime_buffer.h>
 
 namespace iai_boxy_sim
 {
@@ -14,8 +16,7 @@ namespace iai_boxy_sim
       FireHoseController() {}
       ~FireHoseController()
       {
-        // TODO: implement me 
-        // sub_command_.shutdown();
+        subscriber_.shutdown();
       }
 
       bool init(T* hw, ros::NodeHandle &nh)
@@ -45,19 +46,57 @@ namespace iai_boxy_sim
             return false;
           }
         }
-        // TODO: implement me 
+
+        commands_buffer_.writeFromNonRT(std::vector<double>(joint_names_.size(), 0.0));
+
+        subscriber_ = nh.subscribe<giskard_msgs::SemanticFloat64Array>("command", 1, 
+            &FireHoseController::callback, this);
         return true;
       } 
 
       void starting(const ros::Time& time);
 
-      void update(const ros::Time& /*time*/, const ros::Duration& /*period*/)
+      void update(const ros::Time& time, const ros::Duration& period)
       {
-        // TODO: implement me 
+        std::vector<double> & commands = *commands_buffer_.readFromRT();
+        for(unsigned int i=0; i<joint_names_.size(); ++i)
+          joints_[i].setCommand(commands[i]);
       }
 
+    protected:
       std::vector<std::string> joint_names_;
       std::vector< hardware_interface::JointHandle > joints_;
+      realtime_tools::RealtimeBuffer<std::vector<double> > commands_buffer_;
+
+    private:
+      ros::Subscriber subscriber_;
+
+      void callback(const giskard_msgs::SemanticFloat64ArrayConstPtr& msg) 
+      {
+        std::map<std::string, double> index;
+        for (size_t i=0; i<msg->data.size(); ++i)
+          index.insert(std::pair<std::string, double>(msg->data[i].semantics, msg->data[i].value));
+
+        std::vector<double> new_cmd = *(commands_buffer_.readFromNonRT());
+        for (size_t i=0; i<joint_names_.size(); ++i)
+        {
+          typename std::map<std::string, double>::const_iterator it = index.find(joint_names_[i]);
+          if( it != index.end())
+          {
+            new_cmd[i] = it->second;
+          }
+           // TODO: implement else branch with watchdog functionality
+        }
+        
+        commands_buffer_.writeFromNonRT(new_cmd);
+
+//                  if(msg->data.size()!=n_joints_)
+//                        { 
+//                                ROS_ERROR_STREAM("Dimension of command (" << msg->data.size() << ") does not match number of joints (" << n_joints_ << ")! Not executing!");
+//                                      return; 
+//                                          }
+//                      commands_buffer_.writeFromNonRT(msg->data);
+       }
   };
 }
 
